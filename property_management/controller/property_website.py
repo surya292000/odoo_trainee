781763e854563347b00aa39465b9c486dd464993
surya292000 <gayathrysurya2000@gmail.com>
2025-09-15 17:10:54 +0530

    [UPDT] website updation

--- property_management/controller/property_website.py
+++ property_management/controller/property_website.py
@@ -13,110 +13,92 @@ class WebFormController(http.Controller):
         return request.render('property_management.customer_form_template', {
             'properties': properties,
             'rental_types': rental_types,
-            'quantity' : quantity,
+            'quantity': quantity,
         })
-    @http.route('/website/customer/create', type='http', auth='public',
-                website=True, csrf=False)
-    def create_customer(self, **post):
-        partner = request.env.user.partner_id
-        property_val = post.get('property_id')
-        rental_type = post.get('rental_type')
-        start_date = post.get('start_date')
-        end_date = post.get('end_date')
-        # quantity = post.get('total_days')
-        # print(quantity,'quantity')
-        if not property_val:
-            return request.redirect('/webform?error=Please select a property')
-        if not rental_type:
-            return request.redirect('/webform?error=Please select a rental type')
-        if start_date and end_date and start_date > end_date:
-            return request.redirect('/webform?error=End date cannot be before start date')
-        property_id = int(property_val)
-        lease = request.env['property.rental.lease'].sudo().create({
-            'tenant_id': partner.id,
-            'rental_type': rental_type,
-            'start_date': start_date,
-            'end_date': end_date,
-            # 'total_days' : quantity,
-            'property_ids': [Command.create({'property_id': property_id})],
-        })
-        return request.render('property_management.customer_success_template', {
-            'lease': lease,
-        })
+
+    # @http.route('/website/customer/create', type='http', auth='public',
+    #             website=True, csrf=False)
+    # def create_customer(self, **post):
+    #     partner = request.env.user.partner_id
+    #     property_val = post.get('property_id')
+    #     rental_type = post.get('rental_type')
+    #     start_date = post.get('start_date')
+    #     end_date = post.get('end_date')
+    #
+    #     if not property_val:
+    #         return request.redirect('/webform?error=Please select a property')
+    #     if not rental_type:
+    #         return request.redirect('/webform?error=Please select a rental type')
+    #     if start_date and end_date and start_date > end_date:
+    #         return request.redirect('/webform?error=End date cannot be before start date')
+    #
+    #     property_id = int(property_val)
+    #     lease = request.env['property.rental.lease'].sudo().create({
+    #         'tenant_id': partner.id,
+    #         # 'rental_type': rental_type,
+    #         'start_date': start_date,
+    #         'end_date': end_date,
+    #         'property_ids': [Command.create({'property_id': property_id})],
+    #     })
+    #     return request.render('property_management.customer_success_template', {
+    #         'lease': lease,
+    #     })
+
+    @route('/property-property', type='json', auth='public', website=True)
+    def get_property_data(self, **post):
+        """
+        Returns property data for calculations (rent, lease prices, owner info)
+        """
+        properties = request.env['property.details'].sudo().search([])
+        property_data = {}
+
+        for prop in properties:
+            property_data[str(prop.id)] = {
+                'owner': prop.owner_id.name if prop.owner_id else 'N/A',
+                'rent': prop.rent or 0,
+                'lease': prop.legal_amount or 0,
+                'name': prop.name or '',
+            }
+
+        # print('Property data:', property_data)
+        return property_data
 
     @route('/material/submit', type='json', auth='public', website=True)
-    def request_submit(self, **post):
-        """
-        Handles the material request submission dynamically based on field types.
-        """
-        print('post', post)
-        model_name = 'property.rental.lease'
-        model_fields = request.env['ir.model.fields'].sudo().search([('model', '=', model_name)])
-        values = {}
-        for key, val in post.items():
-            field = model_fields.filtered(lambda f: f.name == key)
-            if not field:
-                continue
-            # Process data based on field type
-            if field.ttype == 'many2one':
-                val = int(val) if val else False
-            elif field.ttype == 'one2many':
-                relation_fields = request.env['ir.model.fields'].sudo().search([('model', '=', field.relation)])
-                one2many_lines = []
-                for line in val:
-                    line_data = {}
-                    for sub_key, sub_val in line.items():
-                        sub_field = relation_fields.filtered(lambda f: f.name == sub_key)
-                        if sub_field:
-                            if sub_field.ttype == 'many2one':
-                                sub_val = int(sub_val) if sub_val else False
-                            elif sub_field.ttype in ['integer', 'float']:
-                                sub_val = float(sub_val) if sub_val else 0
-                            elif sub_field.ttype == 'boolean':
-                                sub_val = str(sub_val).lower() in ['true', '1', 'yes']
-                        line_data[sub_key] = sub_val
-                    one2many_lines.append((0, 0, line_data))
-                val = one2many_lines
-            values[key] = val
-        # Create record
-        record = request.env[model_name].sudo().create(values)
-        print('record', record)
-        return {'success': True, 'record_id': record.id}
+    def request_submit(self, abc, data, **post):
+        print('sdfghjknbvcxcvbnmbgvfcdsdfghj')
+        """create a record on backend"""
+        order = []
+        print('ggggg', abc)
+        # for rec in data:
+        print('ggggg2',data)
+        #     dicts = {
+        #         'property_id': rec['property_id'],
+        #         'rental_type': rec['rental_type']
+        #     }
+        #     order.append(dicts)
+        # print(data, 'data')
+        # print(user_data, 'user')
+        # print(post, 'post')
+        # print('post', post)
 
-    # @route('/material/submit', type='json', auth='public', website=True, csrf=False)
-    # def request_submit(self, **post):
-    #     """
-    #     Handles the material request submission dynamically based on field types.
-    #     """
-    #     print('post', post)
-    #     values = {}
-    #
-    #     for key, val in post.items():
-    #         if key == "property_ids":  # special handling for one2many
-    #             one2many_lines = []
-    #             for line in val:
-    #                 line_data = {
-    #                     'property_id': int(line.get('property')) if line.get('property') else False,
-    #                     # 'total_days': int(line.get('quantity') or 0),
-    #                     'rental_type': line.get('rental_type') or False,
-    #                 }
-    #                 one2many_lines.append((0, 0, line_data))
-    #             values['property_ids'] = one2many_lines
-    #         else:
-    #             values[key] = val
-    #     # Create record
-    #     record = request.env['property.rental.lease'].sudo().create(values)
-    #     print('record created:', record)
-    #     return {'success': True, 'record_id': record.id}
-        # return request.render('property_management.customer_success_template', {
-        #     'values': values,
-        # })
+        partner = request.env.user.partner_id.id
+        print(partner,'partner')
+        print(data[0]['rental_type'], 'data type')
 
-    # lease = request.env['property.rental.lease'].sudo().create({
-    #     'tenant_id': partner.id,
-    #     'rental_type': rental_type,
-    #     'start_date': start_date,
-    #     'end_date': end_date,
-    #     # 'total_days' : quantity,
-    #     'property_ids': [Command.create({'property_id': property_id})],
-    # })
+        # property_val = post.get('property_id')
+        # print('property_val',property_val)
+        # rental_type = post.get('rental_type')
+        # print(rental_type,'rental_type')
+        # start_date = post.get('start_date')
+        # print('start_date',start_date)
+        # end_date = post.get('end_date')
+        record = request.env['property.rental.lease'].sudo().create({
+            'tenant_id': partner,
+            'rental_type': data[0]['rental_type'],
+            # 'start_date': start_date,
+            # 'end_date': end_date,
+            'property_ids': [Command.create(data)]
+        })
+        print(record,'record')
+        return record
+        # return True
--- property_management/static/src/js/custom_website.js
+++ property_management/static/src/js/custom_website.js
@@ -1,7 +1,11 @@
 /** @odoo-module **/
+import { rpc } from "@web/core/network/rpc";
 import publicWidget from "@web/legacy/js/public/public_widget";
-import { rpc } from '@web/core/network/rpc';
-console.log("tfd")
+import { renderToElement } from "@web/core/utils/render";
+import { useRef, useState } from "@odoo/owl";
+
+//console.log("tfd")
+
 publicWidget.registry.PropertyRentalLease = publicWidget.Widget.extend({
    selector: "#wrap",
    events: {
@@ -11,145 +15,313 @@ publicWidget.registry.PropertyRentalLease = publicWidget.Widget.extend({
        'click .add_total_project': '_onClickAddMaterial',
        'click .remove_line': '_onClickRemoveLine',
        'click .custom_create': '_onClickSubmit',
-
+       'change select[name="property_id"]': 'onChangeProperty',
+       'change .property_name': '_onClickRow',
+       'change .select_rental_type': '_onClickCalculate',
+//       'change .date': '_onClickCalculate',
+       'click #dismiss': '_onCloseClick',
    },
-   _onClickSubmit: async function (ev) {
-    console.log('submit');
-    console.log('sdfghjkl')
-    ev.preventDefault();
-    const property_ids = [];
-    $('#material_table tbody tr.property_order_line').each(function () {
-        const property_id = $(this).find('select[name="property_id"]').val();
-        const quantityStr = $(this).find('input[name="total_days"]').val();
-        const quantity = parseInt(quantityStr, 10) || 0;
-        const rental_type = $(this).find('select[name="rental_type"]').val();
-        property_ids.push({
-            property: property_id,
-            quantity: quantity,
-            rental_type: rental_type,
-        });
-    });
-    try {
-         let result = await this.rpc("/material/submit", {
-//            'rental_type': rental_type,
-            'start_date': start_date,
-            'end_date': end_date,
-            'property_ids': property_ids
-            // add other form fields here if needed
-        });
-        console.log(result, 'rpc result');
 
-        if (result.success) {
-            alert("Request submitted! ID: " + result.record_id);
-        } else {
-            alert("Submission failed");
-        }
-    } catch (error) {
-        alert("Submission failed: " + error.message);
-    }
-},
+   init() {
+       this._super(...arguments);
+       this.orm = this.bindService("orm");
+   },
 
+   setup() {
+       super.setup();
+       this.location = useService("message");
+   },
+
+   _onChangeDates: function() {
+//       console.log('date')
+       var modal_div = this.el.querySelector('#modal_msg');
+       var msg = this.el.querySelector('.modal-title');
+       var start_date = new Date($('#start_date').val())
+       var end_date = new Date($('#end_date').val())
+       var total_days = this.$el.find('#days_count');
+
+       if (start_date > end_date) {
+           if (modal_div && msg) {
+               msg.innerHTML = "Choose Date Correctly"
+               modal_div.style.display = 'block';
+           } else {
+               alert("End Date must be after Start Date.");
+           }
+           $('#end_date').val("");
+           $('#start_date').val("");
+           $('.total_days').val('');
+           return;
+       }
+       else if (start_date && end_date) {
+           const diffMs = end_date.getTime() - start_date.getTime();
+           const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24)) + 1;
+
+           $('#material_table tbody tr.property_order_line').each(function () {
+//           console.log('dfghjkl;lkbvbnm,')
+               $(this).find('input[name="total_days"]').val(diffDays);
+           });
+
+           if (total_days.length) {
+               total_days.text('Total Days : ' + diffDays);
+           }
+           console.log("Dates changed", $('#start_date').val(), $('#end_date').val(), diffDays);
+       }
+   },
+
+   _onCloseClick: function() {
+       var modal_div = this.el.querySelector('#modal_msg');
+       var msg = this.el.querySelector('.modal-title');
+       var start_date = $('#start_date')
+       var end_date = $('#end_date')
+
+       if (msg && msg.textContent == "Choose Date Correctly") {
+           end_date.val("")
+           start_date.val("")
+           if (modal_div) modal_div.style.display = 'none';
+       }
+       else {
+           if (modal_div) modal_div.style.display = 'none';
+       }
+   },
 
    _onClickAddMaterial: function (ev) {
-       console.log("gyduy")
+//       console.log("gyduy")
+       var modal_div = this.el.querySelector('#modal_msg');
+       var msg = this.el.querySelector('.modal-title');
+       var rows = $('#material_table tbody tr.property_order_line');
        var $new_row = $('#material_table tbody tr.property_order_line:first').clone();
+       var start_date = new Date($('#start_date').val())
+       var end_date = new Date($('#end_date').val())
+       var total_days = this.$el.find('#days_count');
+       const diffMs = end_date.getTime() - start_date.getTime();
+       const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24)) + 1;
+
+       $new_row.removeClass('d-none')
+       $new_row.removeClass('property_order_line');
+       $new_row.addClass('property_order_line');
+       $new_row.find('select[name="property_id"]').val("")
        $new_row.find('input, select').val('');  // Clear input values
-       $new_row.appendTo('#material_table tbody');
-//       console.log('console')
+
+       rows.each(function(index, elem){
+
+           if($(elem).find('select[name="property_id"]').val() == null || $(elem).find('select[name="property_id"]').val() == ""){
+               if (modal_div && msg) {
+                   msg.innerHTML = "Fill the previous row";
+                   modal_div.style.display = 'block';
+               } else {
+                   alert("Fill the previous row");
+               }
+               elem.remove();
+           }
+           else{
+//           console.log(elem)
+               $new_row.insertBefore(rows.eq(0));
+               $new_row.insertBefore(rows.eq(0)).find('input[name="total_days"]').val(diffDays);
+               $new_row.insertBefore(rows.eq(0)).find('.owner').text(" ");
+               $new_row.insertBefore(rows.eq(0)).find('.amount').text(" ");
+               $new_row.insertBefore(rows.eq(0)).find('.sub_amount').text(" ");
+               console.log($new_row.insertBefore(rows.eq(0)),'hhhhhhhhhhhhhhhhhhhhh')
+
+           }
+       });
    },
+
    _onClickRemoveLine: function (ev) {
-       console.log('dfghjkjb')
-       if ($('#material_table tbody tr').length > 1) {
+        if ($('#material_table tbody tr').length > 1) {
            $(ev.target).closest('tr').remove();
        } else {
            alert("You must have at least one material entry.");
        }
    },
 
-   _onChangeDates: function () {
-       console.log('date')
-    const s = $('#start_date').val();
-    const e = $('#end_date').val();
-    if (!s || !e) {
-        $('.total_days').val('');
-        return;
-    }
-    const start = new Date(s);
-    const end = new Date(e);
-    if (end < start) {
-        alert("End Date must be after Start Date.");
-        $('.total_days').val('');
-        return;
-    }
-    // difference in days (inclusive)
-    const diffMs = end.getTime() - start.getTime();
-    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24)) + 1;
-    // fill ALL rows with the computed days
-    $('#material_table tbody tr.property_order_line').each(function () {
-        $(this).find('input[name="total_days"]').val(diffDays);
-//        this.$el.find()
-        console.log("Dates changed", s, e, diffDays);
-    });
-  },
+   _onClickCalculate :  async function () {
+           const data = await rpc('/property-property', {})
+            var $table = this.$el.find('#material_table tbody tr')
+//            console.log('$table',$table)
+            var from_date = new Date(this.$el.find('#start_date').val())
+//            console.log('from_date',from_date)
+            var to_date = new Date(this.$el.find('#end_date').val())
+//            console.log('to_date',to_date)
+            var type = this.$el.find('.select_rental_type').val();
+//            console.log('type',type)
+            var total_days = (to_date - from_date) / 86_400_000;
+            $table.each(function(index, elem){
+                var property = $(elem).find('#property_id').val()
+//                console.log('dfghjkl')
+                var span_owner = $(elem).find('.owner');
+//                console.log('span_owner',span_owner)
+                var span_amount = $(elem).find('.amount');
+//                console.log('span_amount',span_amount)
+                var span_sub_amount = $(elem).find('.sub_amount')
+//                console.log('span_sub_amount',span_sub_amount)
+                if (property){
+//                console.log('hai')
+//                console.log('tppp',type)
+                    span_owner.text(data[property].owner);
+                    console.log('owwww',span_owner.text(data[property].owner))
+                    if(type=='rent'){
+//                    console.log('hello')
+//                    console.log('amou',data[property].rent)
+                        span_amount.text(data[property].rent);
+                        span_sub_amount.text(data[property].rent * total_days);
+                         }
+                    else{
+                        span_amount.text(data[property].lease)
+                        span_sub_amount.text(data[property].lease * total_days)}
+                }
+            })
+           },
 
-//    _onChangeDates: function (ev) {
-//        // read global dates
-//        const s = $('#start_date').val();
-//        const e = $('#end_date').val();
-//
-//        // if either missing, clear values
-//        if (!s || !e) {
-//            $('input[name="total_days"]').val('');
-//            $('#quantity_global').val('');
-//            return;
-//        }
-//
-//        // parse dates
-//        const start = new Date(s);
-//        const end = new Date(e);
-//
-//        // compute milliseconds difference (end - start)
-//        const diffMs = end.getTime() - start.getTime();
-//
-//        if (diffMs < 0) {
-//            alert("End Date must be the same or after Start Date.");
-//            $('input[name="total_days"]').val('');
-//            $('#quantity_global').val('');
-//            return;
-//        }
-//
-//        const msPerDay = 1000 * 60 * 60 * 24; // 86,400,000 ms
-//        const diffDays = Math.floor(diffMs / msPerDay) + 1;
-//
-//        // set every row's total_days input
-//        $('#material_table tbody tr.property_order_line').each(function () {
-//            $(this).find('input[name="total_days"]').val(diffDays);
-//        });
-//
-//        // set global quantity input (if you have one)
-//        $('#quantity_global').val(diffDays);
-//        },
+   _onClickRow : async function(ev){
+           var current_row = $(ev.target)
+           console.log(current_row,'current ')
+           var from_date = new Date(this.$el.find('#start_date').val())
+           var to_date = new Date(this.$el.find('#end_date').val())
+           var total_days = (to_date - from_date) / 86_400_000;
+           var type = this.$el.find('.select_rental_type').val();
+           const data = await rpc('/property-property', {})
+           console.log(data, 'data')
+           var property = current_row.closest('tr').find('#property_id').val()
+                var span_owner = current_row.closest('tr').find('.owner');
+                var span_amount = current_row.closest('tr').find('.amount');
+                var span_sub_amount = current_row.closest('tr').find('.sub_amount');
+//                var quantity = (current_row.closest('tr').find('.total_days');
+//                console.log(quantity, 'quantity');
 
-//        },
+                console.log(property,'property')
+                if (property){
+                    span_owner.text(data[property].owner);
+//                    quantity.text(total_days)
+                    console.log(type,'type')
+                    if(type=='rent'){
+                    console.log('dfghjk')
+                        span_amount.text(data[property].rent);
+                        span_sub_amount.text(data[property].rent * total_days)
+                         }
+                        else{
+                             span_amount.text(data[property].lease)
+                             span_sub_amount.text(data[property].lease * total_days)}
+                }
+           },
 
 
-//   _onChangeType: function (ev) {
-//       var $row = $(ev.target).closest('tr');
-//       if ('start_date' && 'end_date'){
-//       console.log('sdfghj')
+   _onClickSubmit: async function (ev) {
+//       console.log('submit2');
+//       console.log('sdfghjkl')
+       ev.preventDefault();
+        console.log('submit')
+       var modal_div = this.el.querySelector('#modal_msg');
+       var msg = this.el.querySelector('.modal-title');
+       console.log('zdrtgfvghnjkjhgfdsdfg')
+       var start_date = $('#start_date').val();
+       var end_date = $('#end_date').val();
+        console.log('startttttttt')
+       const property_ids = [];
+       let data =[];
+       if(start_date.length==0 || end_date.length==0){
+           if (modal_div && msg) {
+               msg.innerHTML = "Date not provided";
+               modal_div.style.display = 'block';
+           } else {
+               alert("Date not provided");
+           }
+           ev.preventDefault();
+           ev.stopPropagation();
+           return;
+       }
+       $('#material_table tbody tr.property_order_line').each(function () {
+           if(!$(this)[0].classList.contains('d-none')){
+               let row = $(this);
+               const property_id = row.find('select[name="property_id"]').val();
+               console.log('property_id',property_id)
+               const quantityStr = row.find('input[name="total_days"]').val();
+               const quantity = parseInt(quantityStr, 10) || 0;
+               const rental_type = row.find('select[name="rental_type"]').val();
+               console.log(rental_type,'rentalllllllllllllllll')
+
+               data.push({
+               property_id: property_id,
+               rental_type: rental_type,
+               })
+               console.log('data',data)
+
+               if (property_id == null || property_id == ""){
+               console.log('propppppp')
+                   if (modal_div && msg) {
+                       msg.innerHTML = "Completely fill property details"
+                       modal_div.style.display = 'block';
+                   } else {
+                       alert("Completely fill property details");
+                   }
+                   row.remove();
+                   ev.preventDefault();
+               }
+               else{
+                   property_ids.push({
+                       property: property_id,
+//                       quantity: quantity,
+                       rental_type: rental_type,
+                   });
+               }
+           }
+       });
+       console.log('neyhdbedbu',property_ids)
+       if(property_ids.length == 0){
+           if (modal_div && msg) {
+               msg.innerHTML = "Property not selected"
+               modal_div.style.display = 'block';
+           } else {
+               alert("Property not selected");
+           }
+           ev.preventDefault();
+           return;
+       }
+
+       try {
 //       }
-////       $row.find()
-////       } else {
-////           $row.find('.fields').prop('disabled', false);
-////       }
-//   }
+           let result = await rpc("/material/submit", {
+               'abc': start_date,
+               'data': data,
+//               'end_date': end_date,
+//               'property_ids': property_ids
+           });
+           console.log(result, 'rpc result');
 
-////   _onChangeType: function (ev) {
-////       var $row = $(ev.target).closest('tr');
-////       if ($row.find('.operation').val() === "purchase order") {
-////           $row.find('.fields').prop('disabled', true);
-////       } else {
-////           $row.find('.fields').prop('disabled', false);
-////       }
-////   }
+           if (result.success) {
+               alert("Request submitted! ID: " + result.record_id);
+           } else {
+               alert("Submission failed: " + (result.error || "Unknown error"));
+           }
+       } catch (error) {
+           alert("Submission failed: " + error.message);
+       }
+   },
+
+   onChangeProperty: async function(ev) {
+       var modal_div = this.el.querySelector('#modal_msg');
+       var msg = this.el.querySelector('.modal-title');
+       var selected_value = $(ev.target);
+       var $table = $('#material_table tbody tr.property_order_line')
+
+       $table.each(function(index, elem){
+           if(elem !== selected_value.closest('tr')[0] && (!elem.classList.contains('d-none'))){
+               if($(elem).find('select[name="property_id"]').val() == selected_value.val()){
+                   if (modal_div && msg) {
+                       msg.innerHTML = "Property already chosen"
+                       modal_div.style.display = 'block';
+                   } else {
+                       alert("Property already chosen");
+                   }
+                   selected_value.val("")
+                   selected_value.closest('tr').find('.owner').text("")
+                   selected_value.closest('tr').find('.amount').text("")
+                   selected_value.closest('tr').find('.sub_amount').text("")
+               }
+           }
+       });
+   },
+
+   _onChangeType: function (ev) {
+       console.log('Property type changed');
+   }
 });
